a302195ecb4ee735acd93969996a13bb14f529f6
Matteo Valenzi <dilloawallace@hotmail.com>
2024-09-23 21:13:12 +0200

    registrazione completa

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -2,10 +2,17 @@ export class RegisterUserDTO {
   nome: string;
   annoNascita: number;
   genere: string;
+  immagine: string;
+  email: string;
+  password: string;
 
   constructor() {
     this.nome = "";
     this.annoNascita = 2000;
     this.genere = "nb";
+    this.email = "";
+    this.password = "";
+    this.immagine = "nb";
+
   }
 }
